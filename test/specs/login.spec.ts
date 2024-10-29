cae51570f9fe541c8a00363e263c30abc94c3b6d
anhduong1611 <hothianhduong16112002@gmail.com>
2024-10-29 15:28:06 +0700

    Test 14

--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -13,26 +13,26 @@ describe('Login',async()=>{
         await loginPage.login(userData.userExistEmail);
         await accountPage.checkInfoAccount(userData.userExistEmail);
     })
-    it('Login unsuccess with email not existed',async ()=>{
-        await loginPage.login(userData.userNoExistEmail);
-        await loginPage.checkErrorMessage(userData.userNoExistEmail.mess);
-    })
+    // it('Login unsuccess with email not existed',async ()=>{
+    //     await loginPage.login(userData.userNoExistEmail);
+    //     await loginPage.checkErrorMessage(userData.userNoExistEmail.mess);
+    // })
 
-    it('Login unsuccess with all empty field',async()=>{
-        await loginPage.login(userData.userAllEmptyField);
-        await loginPage.checkInputAEmpty();
-    })
-    it('Login unsuccess with the invalid pass format',async()=>{
-        await loginPage.login(userData.userShortPassword);
-        await loginPage.checkErrorMessage(userData.userShortPassword.messLogin);
-    })
-    it('Login unsuccess with the invalid email format',async()=>{
-        await loginPage.login(userData.userInvalidEmailFormat);
-        await loginPage.checkErrorMessage(userData.userInvalidEmailFormat.mess);
-    })
-    it('Login unsuccess with the correct email but with the wrong password',async()=>{
-        await loginPage.login(userData.userExistEmailButCorrectPass);
-        await loginPage.checkErrorMessage(userData.userExistEmailButCorrectPass.mess);
-    })
+    // it('Login unsuccess with all empty field',async()=>{
+    //     await loginPage.login(userData.userAllEmptyField);
+    //     await loginPage.checkInputAEmpty();
+    // })
+    // it('Login unsuccess with the invalid pass format',async()=>{
+    //     await loginPage.login(userData.userShortPassword);
+    //     await loginPage.checkErrorMessage(userData.userShortPassword.messLogin);
+    // })
+    // it('Login unsuccess with the invalid email format',async()=>{
+    //     await loginPage.login(userData.userInvalidEmailFormat);
+    //     await loginPage.checkErrorMessage(userData.userInvalidEmailFormat.mess);
+    // })
+    // it('Login unsuccess with the correct email but with the wrong password',async()=>{
+    //     await loginPage.login(userData.userExistEmailButCorrectPass);
+    //     await loginPage.checkErrorMessage(userData.userExistEmailButCorrectPass.mess);
+    // })
 
 })
